2ed1b8c703d7d3542fc628e089e1594a411f8efa
Richard Blanch <bricerb@gmail.com>
2016-09-29 17:14:39 -0400

    Initial Commit

---
+++ EventContact/src/main/java/tiy/invictus/EventContactApplication.java
@@ -0,0 +1,12 @@
+package tiy.invictus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventContactApplication.class, args);
+	}
+}
---
+++ EventContact/src/test/java/tiy/invictus/EventContactApplicationTests.java
@@ -0,0 +1,16 @@
+package tiy.invictus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventContactApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
